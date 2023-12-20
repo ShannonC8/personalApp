53cd2877d984fa1a7809823a5a923d35a0ff5039
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2023-12-20 12:21:56 -0500

    Navbar fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
 import React, { useRef } from 'react';
-
+import '../styles/Home.css';
 function Home({ AboutRef, ProjectsRef, ContactRef }) {
 
   return (
     <div>
-      <section ref={AboutRef} id="About" style={{ height: '500px', backgroundColor: 'lightblue' }}>
+      <section ref={AboutRef} id="About" className="About">
         Section 1
       </section>
-      <section ref = {ProjectsRef} id="Projects" style={{ height: '500px', backgroundColor: 'lightgreen' }}>
+      <section ref = {ProjectsRef} id="Projects" className="Projects" >
         Section 2
       </section>
-      <section ref = {ContactRef} id="Contact" style={{ height: '500px', backgroundColor: 'lightpink' }}>
+      <section ref = {ContactRef} id="Contact" className="Contact" >
         Section 3
       </section>
       {/* Add more sections as needed */}
