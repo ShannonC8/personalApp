2f673b6b3acb1a8c4479a6e726dc0fd319015f3b
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2023-12-22 23:18:17 -0500

    Skill bar animation

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/Skills.css';
 
 function Skill({ skillName, percent }) {
-  const skillStyle = {
-    width: `${percent}%`,
-    animationDelay: '0.6s' // Adjust the multiplier as needed
-  };
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                } else {
+                    entry.target.classList.remove('show');
+                }
+            });
+        });
 
-  return (
-    <div className="skill-box">
-      <span className="title">{skillName}</span>
-      <div className="skill-bar">
-        <span className="skill-per" style={skillStyle} />
-      </div>
-    </div>
-  );
+        const hiddenElements = document.querySelectorAll('.skill-per');
+        hiddenElements.forEach((el) => observer.observe(el));
+
+        return () => {
+            hiddenElements.forEach((el) => observer.unobserve(el));
+        };
+    }, []); // Empty dependency array to run once when the component mounts
+
+    const skillStyle = {
+        width: `${percent}%`,
+        animationDelay: '0.2s' // Adjust the multiplier as needed
+    };
+
+    return (
+        <div className="skill-box skill-per hidden">
+            <span className="title">{skillName}</span>
+            <div className="skill-bar">
+                <span className={`skill-per ${percent}`} style={skillStyle} />
+            </div>
+        </div>
+    );
 }
 
 export default Skill;
